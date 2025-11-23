ac188a3db8a8fd973f44b7b6836a73e344c8bcce
Ari <ariguillenua@gmail.com>
2025-11-23 22:42:06 +0100

    lo de la politica de cookies

--- js/cookies.js
+++ js/cookies.js
@@ -50,3 +50,39 @@ function actualizarCookie( event ) { // anyade la cookie o la cambia si ya estab
         }
     }
 }
+
+function mostrarEstadoCookies() { // funcion para mostrar el estado de la cookie en la pagina de la politica de las cookies
+    const estadoSection = document.getElementById('estadoCookies'); // primero se saca el section donde se mostrara el estado
+    const cookiesAceptadas = document.cookie.split('cookiesAceptadas=')[1]; // se saca el valor de la cookie del document.cookie
+
+    if (cookiesAceptadas === 'true') { // su si que hay cookies aceptadas entonces se muestra el mensaje de aceptadas
+        estadoSection.innerHTML = 'Cookies ACEPTADAS - Puedes rechazarlas si cambias de opinión';
+        estadoSection.classList.add('aceptadas'); // y se le anyade el css de acpetadas
+        estadoSection.classList.remove('rechazadas'); // y se le quita el de rechazadas por si ya lo tenia de antes
+    } else if (cookiesAceptadas === 'false') { // si hay cokkies rechazadas entonces se muestra el de rechaazadp
+        estadoSection.innerHTML = 'Cookies RECHAZADAS - Puedes aceptarlas si cambias de opinión';
+        estadoSection.classList.add('rechazadas'); // y lo mismo con el css
+        estadoSection.classList.remove('aceptadas');
+    } else { // si no hay cookies directamnete se pone el mensaje de que no hay todavia
+        estadoSection.innerHTML = 'No hay una decisión registrada aún. Usa los botones para elegir tu preferencia';
+        estadoSection.classList.remove('aceptadas', 'rechazadas'); // y se le quitan las dos clases del css
+    }
+}
+
+function aceptarCookies() { // funcion para aceptar cookies
+    let d = new Date(); // defino variable de tipo date
+    let dias = 45; // defino los dias que dura la cookie
+    d.setTime(d.getTime() + (dias*24*60*60*1000)); // le sumo <dias> dias a la fecha
+    let expira = "expires="+ d.toUTCString(); // guardo en formato string UTC esta informacion preparado para la cookie
+    document.cookie = `cookiesAceptadas=true; ${expira}; path=/`; // pone true en la cookie
+    mostrarEstadoCookies(); // y se llama a la funcion para que se actualice en el html de la politica de cookies
+}
+ 
+function rechazarCookies() { // funcion para rechazar cookies
+    let d = new Date(); // defino variable de tipo date
+    let dias = 45; // defino los dias que dura la cookie
+    d.setTime(d.getTime() + (dias*24*60*60*1000)); // le sumo <dias> dias a la fecha
+    let expira = "expires="+ d.toUTCString(); // guardo en formato string UTC esta informacion preparado para la cookie
+    document.cookie = `cookiesAceptadas=false; ${expira}; path=/`; // lo mismo pero con false
+    mostrarEstadoCookies();
+}
