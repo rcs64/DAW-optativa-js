6b1894eaf3deda705cd6171e4a103101dfcd9a0e
rcs64 <rcs64@gcloud.ua.es>
2025-11-23 17:17:53 +0100

    funciona el gitignore?

---
+++ includes/iniciarDB.php
@@ -0,0 +1,9 @@
+<?php
+$db = mysqli_connect('localhost:3306', 'root', '');
+$db->select_db('pisosbd');
+?>
+
+
+<!-- 
+$db = mysqli_connect('localhost', 'u360956521_admin', 'x3uPYWPW:');
+$db->select_db('u360956521_pisosBD'); -->
